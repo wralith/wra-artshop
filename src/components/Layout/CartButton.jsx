3aeafd692c2c94d8ea09cfb960d46a5977eebd0e
Wralith <wralithdev@gmail.com>
2022-04-08 01:28:20 +0300

    Add animation to header cart button

--- src/components/Layout/CartButton.jsx
+++ src/components/Layout/CartButton.jsx
@@ -1,20 +1,37 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import CartContext from "../../store/cart-context";
 
 //TODO Display only icon and popover when hover
 
 export const CartButton = (props) => {
+  const [buttonAnimation, setButtonAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
   const quantityOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
-    
   }, 0);
+  const { items } = cartCtx;
+  const buttonHighlight = buttonAnimation ? "bump" : "";
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonAnimation(true);
+
+    const timer = setTimeout(() => {
+      setButtonAnimation(false);
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
     <button
       onClick={props.onClick}
-      className="flex flex-row items-center gap-2 rounded-full bg-gray-800 p-2 hover:animate-pulse"
+      className={`flex flex-row items-center gap-2 rounded-full bg-gray-800 p-2 hover:animate-pulse ${buttonHighlight}`}
     >
       <span className="mx-2 text-3xl">
         <HiOutlineShoppingCart />
